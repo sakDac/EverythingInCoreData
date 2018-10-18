aee8dd355e04018854459e4ce4900c98d54cbda4
saket bhushan <saket.bhushan46@gmail.com>
2018-10-18 08:14:59 +0530

    First commit

---
+++ EverythingInCoreData/LoginVC.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  EverythingInCoreData
+//
+//  Created by saket bhushan on 18/10/18.
+//  Copyright © 2018 saket bhushan. All rights reserved.
+//
+
+import UIKit
+
+// Things to cover in core data.
+/*
+ * Basics
+ * Relationships
+ * Threadsafety
+ * Multithreading
+ * using multiple context
+ * using multiple db file
+ * using multiple datamodelfile
+ * updation of model version
+ * updation and migration of db
+ * using predicates
+ * optimising predicates
+ * some less known things
+ * core data theory
+ * what makes core data faster
+ */
+
+
+class LoginVC: UIViewController {
+    
+    
+    @IBOutlet weak var userNameTextEdit: UITextField!
+    
+    @IBOutlet weak var passwordTextEdit: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func loginClick(_ sender: UIButton) {
+        
+       let vc = UIStoryboard.init(name: "Main", bundle: Bundle.allBundles.first).instantiateViewController(withIdentifier: "MyProfileVC") as! MyProfileVC
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+        print("clicked")
+    }
+}
+
---
+++ EverythingInCoreData/MyProfileVC.swift
@@ -0,0 +1,34 @@
+//
+//  MyProfileVC.swift
+//  EverythingInCoreData
+//
+//  Created by saket bhushan on 18/10/18.
+//  Copyright © 2018 saket bhushan. All rights reserved.
+//
+
+import UIKit
+
+class MyProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension MyProfileVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+}

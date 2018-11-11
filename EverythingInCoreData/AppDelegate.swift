44a3cece0a43f98179914479ca6f3a8f93c393f5
saket bhushan <saket.bhushan46@gmail.com>
2018-11-11 21:51:16 +0530

    missed changes.

--- EverythingInCoreData/AppDelegate.swift
+++ EverythingInCoreData/AppDelegate.swift
@@ -76,12 +76,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data Saving support
 
     func saveContext () {
-        
-        persistentContainer.performBackgroundTask { (<#NSManagedObjectContext#>) in
-            <#code#>
-        }
-        
-        
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
